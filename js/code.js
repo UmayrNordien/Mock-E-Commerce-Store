ff6bf4977da48ae6e5a926d0af3f7f376585cd3b
UmayrNordien <umayrnordien@gmail.com>
2022-12-06 15:59:45 +0200

    updated array details, not yet displaying

--- js/code.js
+++ js/code.js
@@ -21,44 +21,95 @@ window.onscroll = () => {
 
 const sr = ScrollReveal ({
 	distance: '15px',
-	duration: 2000,
+	duration: 1800,
 	reset: true
 })
 
 //HOME PAGE TEXT DELAY (SCROLL REVEAL)
-sr.reveal('.home-text, .cta-text, .main-contact',{delay: 400, origin:'center'}) //home section, sale banner section and contact section text display delay and display transition (long) coming from the center
+sr.reveal('.home-text, .cta-text, .main-contact',{delay: 100, origin:'center'}) //home section, sale banner section and contact section text display delay and display transition (long) coming from the center
 
 
 
 //////////////////////////////////////
 
 
+// PRODUCT DATA: 
+let products = JSON.parse(localStorage.getItem('products')) ?
+JSON.parse(localStorage.getItem('products')) : [
+	//t-shirts
+    {
+        id: 1,
+        productName: "FadedCrewNeckPurpleT",
+		size: "L",
+		quantity: "2",
+		image:"https://i.postimg.cc/P56qnJ4S/Crew-Neck-1.jpg",
+		price:"R800"
+    },
+	//sweatshirts
+	{
+        id: 2,
+        productName: "GraphicSweatShirt",
+		size: "M",
+		quantity: "3",
+		image:"https://i.postimg.cc/L8tD8d7Y/Graphic-Sweat-Shirt-1.jpg",
+		price:"R690"
+    },
+	//jackets
+	{
+        id: 3,
+        productName: "CarharttBonesJacket",
+		size: "L",
+		quantity: "1",
+		image:"https://i.postimg.cc/1XzWTW5C/Carhartt-Bones-1.jpg",
+		price:"R2200"
+    },
+	//hoodies
+	{
+        id: 4,
+        productName: "NikePurpleFlameHoodie",
+		size: "M",
+		quantity: "4",
+		image:"https://i.postimg.cc/tgNGGMpp/Nike-Purple-Flames.jpg",
+		price:"R2200"
+    },
+	//pants
+	{
+        id: 5,
+        productName: "GraphicSWPants",
+		size: "XL",
+		quantity: "1",
+		image:"https://i.postimg.cc/cCfd6Mnc/275834450-542228100497493-5314424466359396574-n.jpg",
+		price:"R1200"
+    },
+	//other
+	{
+        id: 6,
+        productName: "SkiMask",
+		size: "M",
+		quanintity: "6",
+		image:"https://i.postimg.cc/DfDT3Nt3/Ski-Mask-1.jpg",
+		price:"R600"
+    }
+];
 
-// // Link a card
-// let wrapper = document.querySelector('.card-wrapper');
-// // Fetch data from the api
-// async function fetchData() {
-//     const data = await fetch('https://randomuser.me/api?results=100');
-//     const results = await data.json();
-//     return results.results;
-// }
-// // Read data 
-// async function readData() {
-//     let results = await fetchData();
-//     /*
-//         forEach( function )
-//     */
-//     results.forEach((record)=>{
-//         wrapper.innerHTML += `
-//         <div class="card">
-//             <img src="${record.picture.large}" class="card-img-top" alt="$      {record.name.first}">
-//             <div class="card-body">
-//                 <h5 class="card-title">${record.name.title} ${record.name.first} ${record.name.last}</h5>
-//                 <p class="card-text">Gender: ${record.gender} and am ${record.dob.age} years old</p>
-//             </div>
-//         </div>
-//         `
-//     })
-// }
+let wrapper = document.querySelector('.item');
 
-// readData();
+function displayProducts() {
+	products.forEach((products) => {
+		document.querySelector('#products').innerHTML +=`<div class="card mb-3" style="max-width: 540px;">
+		<div class="row  g-0">
+		  <div class="col-md-4">
+			<img src="${products.image}" class="img-fluid rounded-start">
+		  </div>
+		  <div class="col-md-8">
+			<div class="card-body">
+			  <h5 class="card-title">${products.productName}</h5>
+			  <div class="card-text">${products.image}</div>
+			  <p class="card-text"><small class="text-muted">${products.price}</small></p>
+			</div>
+		  </div>
+		</div>
+	  </div>`
+	})
+};
+displayProducts()
